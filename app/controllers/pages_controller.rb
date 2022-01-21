95dd3b86be600e5d6d18c30160d98a6b1aa9d961
varshajha941 <varshajha941@gmail.com>
2022-01-21 12:36:32 +0530

    Scafold and models

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
 def home
 end
+def index
+    @articles=Article.all
+end
+def show
+    @article=Article.find(params[:id])
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources:students
   root 'pages#home'
   get 'about' ,to:'pages#about'
+  get  'articles',to:'pages#index'
+  get  'articles/:id',to:'pages#show'
 end
+  
